c7d712f0990c284be6ff7f20edff50a2bc4bdb3e
joacokhabie <jkhabie@mail.austral.edu.ar>
2026-05-08 10:32:21 -0300

    Add files via upload

--- exercise_age_check.py
+++ exercise_age_check.py
@@ -4,7 +4,7 @@ def age_check():
 
     Leer una edad y un límite de edad mediante input(). Verificar que ambos números sean
     válidos (positivos), y luego determinar si la persona es mayor de edad comparando
-    con el límite ingresado.
+    con e l límite ingresado.
 
     Si alguno de los números es negativo o cero, imprimir "Entrada invalida".
 
@@ -18,4 +18,17 @@ def age_check():
         Para las entradas "-5" y "18", la salida esperada es:
         Entrada invalida
     """
-    pass
+    edad = int(input())
+    limite = int(input())
+
+    if edad <= 0 or limite <= 0:
+        print("Entrada invalida")
+    elif edad >= limite:
+        print("Eres mayor de edad")
+    else:
+        print("Eres menor de edad")
+
+
+
+
+
--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,23 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
-    pass
+
+    clave = input()
+
+    tiene_numero = (
+        "0" in clave or "1" in clave or "2" in clave or "3" in clave or "4" in clave or
+        "5" in clave or "6" in clave or "7" in clave or "8" in clave or "9" in clave
+    )
+
+    errores = False
+
+    if len(clave) < 8:
+        print("Contraseña muy corta")
+        errores = True
+
+    if not tiene_numero:
+        print("Debe contener un numero")
+        errores = True
+
+    if not errores:
+        print("Contraseña valida")
